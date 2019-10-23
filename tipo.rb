97058ff68ddb5fe0ad071ba83129dc140aca124d
larissa silva <larissanatalii@MacBook-Air-de-larissa.local>
2019-10-23 09:35:49 -0300

    primeiras aulas

---
+++ tipo.rb
@@ -0,0 +1,25 @@
+# -- Tipos Ruby -- 
+
+# o tipo da variavel é definido no momento em que a mesmoa recebe o valor 
+
+produto = 'Macbook air' #Variavel se tornou string
+preco = 9999.99
+quantidade = 10 
+disponivel = true 
+
+puts 'valor da variavel'
+puts produto
+puts preco
+puts quantidade
+puts disponivel 
+
+puts 'tipo da variavel'
+puts produto.class
+puts preco.class
+puts quantidade.class  
+puts disponivel.class 
+
+puts 'trocando valor da variavel quantidade de numero para texto'
+quantidade = '9'
+puts quantidade.class 
+
