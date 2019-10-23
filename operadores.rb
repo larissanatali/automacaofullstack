08f0cdf6b310f4fcc7f29941d4b83bb248d72cc3
larissa silva <larissanatalii@MacBook-Air-de-larissa.local>
2019-10-23 13:52:30 -0300

    mensagem

---
+++ operadores.rb
@@ -0,0 +1,28 @@
+# Operadores Matematicos 
+
+puts 'Informe o número 1:'
+
+n1 = 0 
+n2 = 0 
+total = 0 
+
+puts 'Informe o número 1: '
+n1 = gets.chomp.to_i
+
+puts 'Informe o número 2: '
+n2 = gets.chomp.to_i
+
+total = n1 + n2
+total = n1 - n2
+total = n1 / n2
+total = n1 % n2
+total = n1 ** n2
+
+puts total
+
+# Operadores Comparação 
+
+v1 = 10
+v2 = 11 
+
+puts v1 > v2 
