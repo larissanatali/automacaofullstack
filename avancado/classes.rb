e55a80a68246b9031a6acced6ed5651caa97c806
larissa silva <larissanatalii@MacBook-Air-de-larissa.local>
2019-12-06 16:39:12 -0300

    construtores

--- avancado/classes.rb
+++ avancado/classes.rb
@@ -1,16 +1,27 @@
 class Conta 
 
-    attr_accessor :saldo
+    attr_accessor :saldo, :nome
+
+    def initialize(nome)
+        self.saldo = 0.0
+        self.nome = nome 
+    end
 
     def deposita(valor)
         self.saldo += valor 
         puts 'Depositando a quantia de ' + valor.to_s + ' reais.' #convertendo variavel 
-        puts "Depositando a quantia de #{valor} reais." #interpolação de string 
+        puts "Depositando a quantia de #{valor} reais na conta de #{self.nome}." #interpolação de string 
     end 
 end
 
-c = Conta.new 
-c.saldo = 0.0  
+c = Conta.new('Larissa')
+#c.saldo = 0.0  
 c.deposita(100.00)
+puts c.nome
+puts c.saldo
+
+
+c.deposita(10.00)
+puts c.nome
 puts c.saldo
 
