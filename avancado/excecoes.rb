320e3d38777f6867a2879927968b3e1333ca4044
larissa silva <larissanatalii@MacBook-Air-de-larissa.local>
2019-12-09 10:22:47 -0300

    pasta

---
+++ avancado/excecoes.rb
@@ -0,0 +1,31 @@
+# #Sem erro 
+# begin
+#     #Devo tentar alguma coisa 
+#     file = File.open('./ola.txt')
+#     if file 
+#         puts file.read
+#     end
+# rescue Exception => e 
+#     #Obter um possivel erro 
+#     puts e.message
+#     puts e.backtrace
+# end
+
+
+# #try caty do java, trata exceções 
+
+# #Com erro
+# begin
+#     #Devo tentar alguma coisa 
+#     file = File.open('./ola')
+#     if file 
+#         puts file.read
+#     end
+# rescue Exception => e 
+#     #Obter um possivel erro 
+#     puts e.message
+#     puts e.backtrace
+# end
+
+
+# #try caty do java, trata exceções 
